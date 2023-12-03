cbfb70fd67a8bc21046faab6635ff93df2dee185
Gusthiedo <guidogusthiabadi@yahoo.com>
2023-12-03 23:01:12 +0700

    first

--- controllers/register.js
+++ controllers/register.js
@@ -1,40 +1,38 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+  console.log("Request Body:", req.body);
+
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json("incorrect form submission");
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  console.log(hash);
+  db.transaction((trx) => {
+    trx
+      .insert({
         hash: hash,
-        email: email
+        email: email,
       })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        console.log(loginEmail[0]);
+        return trx("users")
+          .returning("*")
           .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
-            email: loginEmail[0].email,
+            email: loginEmail[0],
             name: name,
-            joined: new Date()
+            joined: new Date(),
           })
-          .then(user => {
+          .then((user) => {
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-}
-
-module.exports = {
-  handleRegister: handleRegister
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json(err.message));
 };
 
-
+module.exports = {
+  handleRegister: handleRegister,
+};
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,53 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    console.log("Incorrect form submission");
+    return res.status(400).json("incorrect form submission");
   }
-  db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
+
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      if (data.length === 0) {
+        console.log("User not found");
+        return res.status(400).json("wrong credentials");
+      }
+
       const isValid = bcrypt.compareSync(password, data[0].hash);
+      console.log("Stored hash:", data[0].hash); // Log the stored hash
+      console.log("Entered password:", password); // Log the entered password
+      console.log("Password comparison result:", isValid);
+
       if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            if (user.length > 0) {
+              console.log("User signed in successfully:", user[0]);
+              res.json(user[0]);
+            } else {
+              console.log("User not found");
+              res.status(400).json("user not found");
+            }
           })
-          .catch(err => res.status(400).json('unable to get user'))
+          .catch((err) => {
+            console.error("Error getting user:", err);
+            res.status(400).json("unable to get user");
+          });
       } else {
-        res.status(400).json('wrong credentials')
+        console.log("Wrong credentials");
+        res.status(400).json("wrong credentials");
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
-}
+    .catch((err) => {
+      console.error("Error checking credentials:", err);
+      res.status(400).json("wrong credentials");
+    });
+};
 
 module.exports = {
-  handleSignin: handleSignin
-}
+  handleSignin: handleSignin,
+};
